314c190c306bb36b27ef804607a25ccb0484f21b
Craig West <cawest1221@gmail.com>
2014-05-26 18:02:57 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import time
+import argparse
+import requests
+import threading
+from bs4 import BeautifulSoup as bs
+
+repins = 0
+likes = 0
+
+def thread_handler(link):
+    global repins
+    global likes
+
+    r = requests.get(link)
+    soup = bs(r.text)
+
+    like_em = soup.find_all('em', {'class' : 'likeCountSmall'})
+    repin_em = soup.find_all('em', {'class' : 'repinCountSmall'})
+
+    if like_em:
+        for entry in like_em:
+            likes += int(entry.get_text().strip())
+
+    if repin_em:
+        for entry in repin_em:
+            repins += int(entry.get_text().strip())
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Find the number of likes and repins of a Pinterest user')
+    parser.add_argument('-u', '--username', required=True, help='Pinterest username')
+    args = parser.parse_args()
+
+    username = args.username
+    url = "http://www.pinterest.com/{}/".format(username)
+    ignore = ['boards', 'pins', 'likes', 'followers', 'following']
+
+    try:
+        r = requests.get(url)
+        soup = bs(r.text)
+
+        for links in soup.find_all('a'):
+            suffix = links.get('href')
+            if username in suffix and suffix[11:-1] not in ignore:
+                link = 'http://www.pinterest.com' + suffix
+                t = threading.Thread(target=thread_handler, args=[link])
+                t.start()
+                t.join()
+
+        time.sleep(2)
+        print("Likes: {}".format(likes))
+        print("Repins: {}".format(repins))
+
+    except Exception as e:
+        print(str(e))
